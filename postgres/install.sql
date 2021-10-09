564c48d6708b0147385708bb679d121c42f7f05d
Nikolaev Tomov <nikolaev.github@yahoo.com>
2021-10-09 10:24:05 +0100

    Update sql script

--- postgres/install.sql
+++ postgres/install.sql
@@ -38,8 +38,6 @@ CREATE SCHEMA recommender
         keywords TEXT
     );
 
--- SET search_path TO recommender, public;
-
 SELECT 'Copying data into recommender.movies_metadata';
 \copy recommender.movies_metadata(adult,belongs_to_collection,budget,genres,homepage,id,imdb_id,original_language,original_title,overview,popularity,poster_path,production_companies,production_countries,release_date,revenue,runtime,spoken_languages,status,tagline,title,video,vote_average,vote_count) FROM 'data/clean_movies_metadata.csv' DELIMITER E',' CSV HEADER;
 
